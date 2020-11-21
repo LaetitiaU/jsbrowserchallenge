6d03643539b4c45adfa9404b6ec5e1e92075e6d9
LaetitiaU <laetitia.usai@gmail.com>
2020-11-21 20:26:08 +0100

    first commit

---
+++ imageorderjs.js
@@ -0,0 +1,12 @@
+const paramoteurs = document.querySelector("#paramoteur");
+paramoteurs.addEventListener('click', (event) => {
+    multiaxe.style.display = 'none'
+    // console.log(event);
+    // console.log(event.currentTarget);
+});
+
+const multi = document.querySelector(".ul multiaxe");
+
+multi.addEventListener('click', (event) => {
+
+});
